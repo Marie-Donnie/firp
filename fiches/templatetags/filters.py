9d25373e031e3df005455bf4f8bff2c187cd8590
Marie-Donnie <delavergnemarie@gmail.com>
2017-06-14 16:07:44 +0200

    Nice display of objet price

---
+++ fiches/templatetags/filters.py
@@ -0,0 +1,34 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def prix(value):
+    p_or = value // 10000
+    reste = value % 10000
+    p_argent = reste // 100
+    reste = reste % 100
+    p_cuivre = reste
+    return p_or, p_argent, p_cuivre
+
+
+@register.filter
+def p_or(value):
+    p_or = value // 10000
+    return p_or
+
+
+@register.filter
+def p_argent(value):
+    reste = value % 10000
+    p_argent = reste // 100
+    return p_argent
+
+
+@register.filter
+def p_cuivre(value):
+    reste = value % 10000
+    reste = reste % 100
+    p_cuivre = reste
+    return p_cuivre
