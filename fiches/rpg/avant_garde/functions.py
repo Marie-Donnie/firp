bc54f1fa606ef32780dccbe93bfc66de0f1a32b1
Marie-Donnie <delavergnemarie@gmail.com>
2017-09-08 10:39:07 +0200

    Added apothecary and crossbowman functions

--- fiches/rpg/avant_garde/functions.py
+++ fiches/rpg/avant_garde/functions.py
@@ -33,3 +33,74 @@ def calcul_base(perso_id):
         pv_max = max(pv_max - 1, 0)
 
     return force, endu, perce, agi, intell, charisme, force_men, pv_max, cap_combat, cap_tir
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%% FANTASSIN %%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+def calcul_fant(perso_id):
+    fantassin = Fantassin.objects.get(pk=perso_id)
+    perso = fantassin.perso
+    force, endu, perce, agi, intell, charisme, force_men, pv_max, cap_combat, cap_tir = calcul_base(perso.id)
+    if fantassin.athle_resi == 2:
+        force = min(force + 1, 10)
+    elif fantassin.athle_resi == 3:
+        endu = min(endu + 1, 10)
+    if fantassin.epe_craned == 2:
+        cap_combat = min(cap_combat + 1, 10)
+    elif fantassin.epe_craned == 3:
+        pv_max = min(pv_max + 2, 10)
+    if fantassin.feroce_impla == 2:
+        force = min(force + 1, 10)
+        cap_combat = min(cap_combat + 1, 10)
+    elif fantassin.feroce_impla == 3:
+        endu = min(endu + 1, 10)
+        pv_max = min(pv_max + 2, 10)
+    if perso.niveau >= 3:
+        force_men = min(force_men + 1, 10)
+    if perso.niveau >= 4:
+        force_men = min(force_men + 1, 10)
+    if perso.niveau >= 5:
+        force_men = min(force_men + 1, 10)
+
+    return force, endu, perce, agi, intell, charisme, force_men, pv_max, cap_combat, cap_tir
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%% APOTHCAIRE %%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+def calcul_apo(perso_id):
+    apo = Apothicaire.objects.get(pk=perso_id)
+    perso = apo.perso
+    force, endu, perce, agi, intell, charisme, force_men, pv_max, cap_combat, cap_tir = calcul_base(perso.id)
+    if perso.niveau >= 1:
+        intell = min(intell + 1, 10)
+    if perso.niveau >= 2:
+        force_men = min(force_men + 1, 10)
+    if perso.niveau >= 3:
+        force_men = min(force_men + 1, 10)
+    if perso.niveau >= 4:
+        force_men = min(force_men + 1, 10)
+    if perso.niveau >= 5:
+        force_men = min(force_men + 1, 10)
+    if apo.guerisseur:
+        intell = min(intell + 1, 10)
+    if apo.scalpel_fioles == 2:
+        intell = min(intell + 1, 10)
+        apo.chirurgie = min(apo.chirurgie + 30, 100)
+    elif apo.scalpel_fioles == 3:
+        cap_tir = min(cap_tir + 1, 10)
+    if apo.guerisseur_enfum == 2:
+        intell = min(intell + 1, 10)
+        force_men = min(force_men+ 1, 10)
+    if apo.guerisseur_enfum == 3:
+        cap_tir = min(cap_tir + 1, 10)
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%% APOTHCAIRE %%%%%%%%%%%%%%%%%%%%%%%%%%%%% #
+
+def calcul_arba(perso_id):
+    arba = Arbaletrier.objects.get(pk=perso_id)
+    perso = arba.perso
+    if perso.niveau >= 3:
+        force_men = min(force_men + 1, 10)
+    if perso.niveau >= 5:
+        force_men = min(force_men + 1, 10)
